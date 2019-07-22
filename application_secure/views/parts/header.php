dd204be381eeda1db041f845bb9c7c0727f97396
Nickolay Shapoval <nikolay@dp360crm.com>
2019-07-22 08:34:59 +0300

    Пустой проект, но всё разнесено по файлам

---
+++ application_secure/views/parts/header.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Фаты на свадьбу</title>
+
+    <link rel="stylesheet" type="text/css" href="/assets/styles.css"/>
+</head>
+<body>
+    <div id="header">
+        <ul class="nav-menu">
+            <li>
+                <a>Виды фат</a>
+            </li>
+            <li>
+                <a>Как выбрать</a>
+            </li>
+            <li>
+                <a>О нас</a>
+            </li>
+        </ul>
+    </div>
